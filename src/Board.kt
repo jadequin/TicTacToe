def85d699251919b195d70b718f29a437bcd2e4b
Till Nitsche <till.nitsche@mni.thm.de>
2020-05-16 22:14:19 +0200

    comment refactoring

--- src/Board.kt
+++ src/Board.kt
@@ -15,40 +15,34 @@ class Board(val board: List<Int> = listOf(0,0,0,0,0,0,0,0,0),
             val turn: Int = +1
 ) {
 
-    //Returns a list with all possible moves as indices of the board.
-    //The moves will also be shuffled for a higher random factor in further methods
+    //Returns: List with all possible moves as indices of the board.
+    //The moves will also be shuffled for a higher random factor in further usages
     private fun listPossibleMoves() = board.mapIndexed { index, i -> if(i == 0) index else null }.filterNotNull().shuffled()
 
-    fun makeMove(pos: Int): Board {
-        //assert(pos >= 0 && pos < board.size && board.elementAt(pos) == 0) //with assert -> wrong syntax highlighting on github
-        return Board(
-                board = board.mapIndexed { index, i -> if(index == pos) turn else i},
-                turn = -turn
-        )
-    }
+    fun makeMove(pos: Int) = Board(board = board.mapIndexed { index, i -> if(index == pos) turn else i}, turn = -turn)
 
     fun makeBestMove(): Board {
+        val moveResults = mutableListOf<Pair<Int, Int>>()
 
-        val moveResults = mutableListOf<Pair<Int, Int>>() //contains:   1. board position of the move, 2. result
-
-        //TODO: For-Loop funktionalisieren ? :P
         for(move in listPossibleMoves()) {
-            if(makeMove(move).result() == turn) //if a move is a winning move just return it
+            if(makeMove(move).result() == turn)
                 return makeMove(move)
 
-            val result = minimax(makeMove(move)) //calculates the result of the given move
+            val result = minimax(makeMove(move))
             moveResults.add(Pair(move, result))
         }
 
-        moveResults.filter { it.second == turn } //filter only the good results (they match with the current players turn)
+        //filter only the 'good' results
+        moveResults.filter { it.second == turn }
 
-        if(moveResults.isNotEmpty()) //just pick the first move (already shuffled in listPossibleMoves)
-            return makeMove(moveResults[0].first)
-        else //means there is no winning option anymore, so just pick a random move
-            return makeMove(listPossibleMoves().random())
+        return if(moveResults.isNotEmpty())
+            makeMove(moveResults[0].first)
+        else
+            makeMove(listPossibleMoves().random())
+            //no winning option anymore, pick a random move
     }
 
-    //Returns a list with the winning row if there is one otherwise null
+    //Returns: List with the winning row if there is one otherwise null
     private fun threeInARow(): List<Int>? {
         val rows = listOf(listOf(0,1,2), listOf(3,4,5), listOf(6,7,8), listOf(0,3,6), listOf(1,4,7), listOf(2,5,8), listOf(0,4,8), listOf(2,4,6))
         rows.forEach {
@@ -61,12 +55,8 @@ class Board(val board: List<Int> = listOf(0,0,0,0,0,0,0,0,0),
     fun isGameOver() = !threeInARow().isNullOrEmpty() || listPossibleMoves().isEmpty()
     fun playerXTurn() = turn == 1 //method for game representation
 
-    //Returns 1 if playerX won, -1 if playerY won and 0 if it's a tie
-    //if the method is called before game end it will also return 0
+    //Returns 1 if playerX won, -1 if playerY won and 0 if it's a tie or the game is not over
     fun result(): Int = if(threeInARow().isNullOrEmpty()) 0 else -turn
-
-
-
 
 
     //TODO: alpha-beta-pruning
@@ -83,7 +73,6 @@ class Board(val board: List<Int> = listOf(0,0,0,0,0,0,0,0,0),
         return bestEval
     }
 
-
     override fun toString(): String {
         var cnt = 1
         return board.joinToString(prefix = "-------\n|", postfix = "-------", separator = "|",
@@ -93,5 +82,4 @@ class Board(val board: List<Int> = listOf(0,0,0,0,0,0,0,0,0),
                 }//new line with every 3rd position
         )
     }
-
-} 
+}
