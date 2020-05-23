5970f4be7b127f3686935c55579b495cdd27d9ae
Till Nitsche <till.nitsche@mni.thm.de>
2020-05-23 19:15:10 +0200

    implemented hashtable

--- src/App.kt
+++ src/App.kt
@@ -2,30 +2,59 @@
 /*
     2020-05-16
  */
+import java.lang.NumberFormatException
 import java.util.*
-import kotlin.concurrent.schedule
+import kotlin.math.pow
 
-fun example() {
-    var board = Board(listOf(0,0,0,0,0,0,0,0,0)) //new clean board
+fun playGame() {
     val reader = Scanner(System.`in`)
+    var b = Board()
 
-    println(board)
-    while(!board.isGameOver()) {
-        board = board.bestMove()
-        println(board)
+    println("\n".repeat(5)) //setup console display
 
-        print("Your Turn - Enter a valid number (1-9): ")
-        val input:Int = reader.nextInt()
-        board = board.makeMove(input - 1)
-        println(board)
+    while(true) {
+        println(if(b.isPlayerXTurn()) "Player X Turn" else "Player O Turn")
+        println(b)
+        println("[ENTER] = AI makes a best move\t" +
+                " [1-9]  = make your own move\t" +
+                "  [b]   = undo move\t" +
+                "  [r]   = reset game\t" +
+                " [q/e]  = quit game\t")
 
+        val input = reader.nextLine()
+
+        if(input.isEmpty() && !b.isGameOver())
+            b = b.bestMove()
+        else if(input.startsWith("q") || input.startsWith("e"))
+            return
+        else if(input.startsWith("b"))
+            if(b == Board())
+                println("Can't undo the origin state")
+            else
+                b = b.undoMove()
+        else if(input.startsWith("r"))
+            b = Board()
+        else if(!b.isGameOver()){
+            val num: Int
+            try {
+                num = input.toInt()
+                if(b.isLegalMove(num - 1))
+                    b = b.makeMove(num - 1)
+                else
+                    println("Try a valid move!")
+            } catch(nfe: NumberFormatException) {
+                println("Try again and enter a valid keystroke")
+            }
+        }
+        if(b.isGameOver())
+            println(if(b.result() == 1) "Player X won the game" else if(b.result() == -1) "Player O won the game" else "Tie")
+
+        println("\n".repeat(2))
     }
-    println(board)
 }
 
+
+
 fun main() {
-    var b = Board()
-    println(b)
-    b = b.bestMove()
-    println(b)
+    playGame()
 }
--- src/Board.kt
+++ src/Board.kt
@@ -1,4 +1,5 @@
 import kotlin.math.*
+
 /*
          Board is a 1D-Array with switching turns (+1 for Player X' Turn, -1 for Player O's Turn)
          0 = empty  ' '         -1 = Player 'O'             1 = Player 'X'
@@ -6,20 +7,38 @@ import kotlin.math.*
 
 class Board(private val fields: List<Int> = listOf(0,0,0,0,0,0,0,0,0), private val turn: Int = +1, private val history: List<Board> = listOf()) {
 
+    companion object {
+        private val hashBoard = hashMapOf<Board, Int>()
+        init {
+            Board().fillMapWithNegaMax()
+        }
+    }
+
     fun makeMove(pos: Int) = Board(fields.take(pos) + listOf(turn) + fields.takeLast(fields.size - pos - 1), -turn, history.plus(this))
 
     fun undoMove() = history.last()
 
-    fun bestMove(): Board = minimax(this).first
-
-    fun isGameOver() = !threeInARow().isNullOrEmpty() || possibleMoves().isEmpty()
+    fun bestMove() = nextMoves()
+        .reduce {
+            acc, move ->
+            if(hashBoard[move]!! > hashBoard[acc]!!)
+                move
+            else
+                acc
+        }//TODO: Filter alle besten Züge heraus und wähle einen zufälligen Zug
 
     //Returns 1 if playerX won, -1 if playerY won and 0 if it's a tie or the game is not over
-    private fun result(): Int = if(threeInARow().isNullOrEmpty()) 0 else -turn
+    fun result(): Int = if(threeInARow().isNullOrEmpty()) 0 else -turn
 
-    private fun possibleMoves(): List<Board> = fields.mapIndexed { index, i -> if(i == 0) makeMove(index) else null }.filterNotNull()
+    fun isGameOver() = outOfMoves() || result() != 0
 
-    private fun winningMove(): Board? = possibleMoves().filter { it.result() == turn }.randomOrNull()
+    fun isLegalMove(pos: Int) = fields[pos] == 0
+
+    fun isPlayerXTurn() = turn == 1
+
+    private fun outOfMoves() = nextMoves().isEmpty()
+
+    private fun nextMoves(): List<Board> = fields.mapIndexed { index, i -> if(i == 0) makeMove(index) else null }.filterNotNull()
 
     //Returns: List with the winning row if there is one otherwise null
     private fun threeInARow(): List<Int>? {
@@ -28,23 +47,39 @@ class Board(private val fields: List<Int> = listOf(0,0,0,0,0,0,0,0,0), private v
         return null
     }
 
-    private fun minimax(board: Board): Pair<Board, Int> {
-        val winner = board.winningMove()
-        if(winner != null)
-            return Pair(winner, winner.result())
-
-        var bestOption = Pair(board, Int.MAX_VALUE * -board.turn)
-        board.possibleMoves().forEach {
-            val option = minimax(it)
-            if(option.second * turn > bestOption.second * turn)
-                bestOption = option
+    private fun fillMapWithNegaMax(): Int {
+        if(isGameOver()) {
+            hashBoard[this] = result() * -turn
+            return result() * -turn
         }
-        return bestOption
+        val worst = nextMoves().fold(Int.MAX_VALUE) {worst, move -> min(worst, -move.fillMapWithNegaMax())}
+        hashBoard[this] = worst
+        return worst
     }
 
     override fun toString(): String {
         var cnt = 1
         return fields.joinToString(separator = "|", prefix = "-------\n|", postfix = "-------",
                 transform = { (if(it == -1) "O" else if(it == 1) "X" else " ") + (if(cnt++ % 3 == 0) "|\n" else "") })
+    }
+
+    override fun hashCode(): Int {
+        //duplicates from every rotation or mirroring
+        val duplicates = listOf(listOf(0,1,2,3,4,5,6,7,8), listOf(6,3,0,7,4,1,8,5,2), listOf(8,7,6,5,4,3,2,1,0), listOf(2,5,8,1,4,7,0,3,6), //<--- Bug des Jahres!! 1 1/2 Tage Dauer statt 2,5,8 <- 2,5,6
+                                listOf(6,7,8,3,4,5,0,1,2), listOf(8,5,2,7,4,1,6,3,0), listOf(2,1,0,5,4,3,8,7,6), listOf(0,3,6,1,4,7,2,5,8))
+        val hashVal = duplicates.fold(Int.MAX_VALUE) {
+            best, i ->
+            min(best, i.foldIndexed(0) {
+                index, acc, j ->
+                acc + (if(fields[index] == -1) 2 else fields[index]) * (3.0.pow(j).toInt())
+            })
+        }
+        return hashVal * turn
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return other.hashCode() == this.hashCode()
     }
 }
